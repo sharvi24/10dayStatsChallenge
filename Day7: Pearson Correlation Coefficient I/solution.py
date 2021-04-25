910c68618b9b7b624941ce33e7343ea3d38464c3
sharvi24 <43946878+sharvi24@users.noreply.github.com>
2021-04-25 23:00:00 +0530

    Update solution.py

--- Day7: Pearson Correlation Coefficient I/solution.py
+++ Day7: Pearson Correlation Coefficient I/solution.py
@@ -1,14 +1,13 @@
-a=[95,85,80,70,60]
-b=[85,95,70,65,70]
+n = int(input())
+arrX = list(map(float,input().split()))
+arrY = list(map(float,input().split()))
 
-sum_a=sum(a)
-mean_a=sum_a/5
-sum_b=sum(b)
-mean_b=sum_b/5
+meanX=sum(arrX)/n
+meanY=sum(arrY)/n
+stdx = (sum([(i - meanX)**2 for i in arrX]) / n)**0.5
+stdy = (sum([(i - meanY)**2 for i in arrY]) / n)**0.5
 
-x_square=sum([i**2 for i in a])
-xy=sum([a[i]*b[i] for i in range(5)])
+covariance = sum([(arrX[i] - meanX) * (arrY[i] -meanY) for i in range(n)])
+coefficient=covariance/(n*stdx*stdy)
 
-b= (5*xy-(sum_a*sum_b))/(5*x_square - (sum_a)**2)
-a=mean_b -b*mean_a
-print(round(a + b*80,3))
+print(round(coefficient,3)) 
