5f9d6daf826eef74e1371e9a6081e96d55e8e32f
sharvi24 <43946878+sharvi24@users.noreply.github.com>
2021-04-25 22:54:41 +0530

    Update solution.py

--- Day1: Interquartile Range/solution.py
+++ Day1: Interquartile Range/solution.py
@@ -1,18 +1,33 @@
-a=1.09
-b=1
-p=a/(a+b)
-q=b/(a+b)
+#!/bin/python3
 
-def factorial(z):
-    n=1
-    for i in range(1,z+1):n=n*i
-    return n 
+import math
+import os
+import random
+import re
+import sys
 
-def perm(x,y):
-    return factorial(x)/(factorial(x-y)*factorial(y))    
+# Complete the 'interQuartile' function below.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY values
+#  2. INTEGER_ARRAY freqs
 
-prob=0    
-for i in range(3,7):
-    prob=prob+perm(6,i)*(p**i)*(q**(6-i))
+def median(array):
+    n=len(array)
+    if n%2!=0: return array[(n-1)//2]
+    else: return (array[(n-1)//2]+array[n//2])/2
 
-print(round(prob,3)) 
+def interQuartile(values, freqs):
+    n=sum(freqs)
+    newarr=[]
+    for i in range(len(freqs)):
+        for f in range(freqs[i]): newarr.append(values[i])
+    newarr.sort()  
+    print("{:.1f}".format(median(newarr[(n+1)//2:]) - median(newarr[:(n//2)])))      
+        
+    
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    val = list(map(int, input().rstrip().split()))
+    freq = list(map(int, input().rstrip().split()))
+    interQuartile(val, freq)
