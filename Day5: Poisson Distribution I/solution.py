51fb978d561ae7ab58f9aed90aff20c76dff859c
sharvi24 <43946878+sharvi24@users.noreply.github.com>
2021-04-29 11:43:45 +0530

    Update solution.py

--- Day5: Poisson Distribution I/solution.py
+++ Day5: Poisson Distribution I/solution.py
@@ -1,18 +1,11 @@
-a=1.09
-b=1
-p=a/(a+b)
-q=b/(a+b)
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+lambda1=2.5
+k=5
 
 def factorial(z):
     n=1
     for i in range(1,z+1):n=n*i
     return n 
 
-def perm(x,y):
-    return factorial(x)/(factorial(x-y)*factorial(y))    
-
-prob=0    
-for i in range(3,7):
-    prob=prob+perm(6,i)*(p**i)*(q**(6-i))
-
-print(round(prob,3)) 
+print(round((lambda1**k)* math.exp(-lambda1)/factorial(k),3))
