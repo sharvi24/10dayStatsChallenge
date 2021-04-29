85e57d903f0a2c9774b1023f00b8a5a7cd3e4fc2
sharvi24 <43946878+sharvi24@users.noreply.github.com>
2021-04-29 11:41:11 +0530

    Update solution.py

--- Day6: Central Limit Theorem II/solution.py
+++ Day6: Central Limit Theorem II/solution.py
@@ -1,14 +1,9 @@
-a=[95,85,80,70,60]
-b=[85,95,70,65,70]
+import math
+mean, std = 100*2.4, 10*2
+#print(mean)
 
-sum_a=sum(a)
-mean_a=sum_a/5
-sum_b=sum(b)
-mean_b=sum_b/5
+def cdf(x):
+    return 0.5 * (1 + math.erf((x - mean) / (std * (2 ** 0.5))))
 
-x_square=sum([i**2 for i in a])
-xy=sum([a[i]*b[i] for i in range(5)])
-
-b= (5*xy-(sum_a*sum_b))/(5*x_square - (sum_a)**2)
-a=mean_b -b*mean_a
-print(round(a + b*80,3))
+# Less than 19.5
+print('{:.4f}'.format(cdf(250)))
