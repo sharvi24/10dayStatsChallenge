a418e0e3f608072131618ef7956d5268b2c89555
sharvi24 <43946878+sharvi24@users.noreply.github.com>
2021-04-29 13:25:20 +0530

    Update solution.py

--- Day5: Normal Distribution I/solution.py
+++ Day5: Normal Distribution I/solution.py
@@ -1,14 +1,21 @@
-a=[95,85,80,70,60]
-b=[85,95,70,65,70]
+# from scipy.stats import norm
 
-sum_a=sum(a)
-mean_a=sum_a/5
-sum_b=sum(b)
-mean_b=sum_b/5
+# prob1=norm(loc = 20 , scale = 2).cdf(19.5)
 
-x_square=sum([i**2 for i in a])
-xy=sum([a[i]*b[i] for i in range(5)])
+# cdf_upper_limit = norm(loc = 20 , scale = 2).cdf(22)
+# cdf_lower_limit = norm(loc = 20 , scale = 2).cdf(20) 
+# prob2 = cdf_upper_limit - cdf_lower_limit
 
-b= (5*xy-(sum_a*sum_b))/(5*x_square - (sum_a)**2)
-a=mean_b -b*mean_a
-print(round(a + b*80,3))
+# print(round(prob1,3))
+# print(round(prob2,3))
+
+import math
+mean, std = 20, 2
+
+def cdf(x):
+    return 0.5 * (1 + math.erf((x - mean) / (std * (2 ** 0.5))))
+
+# Less than 19.5
+print('{:.3f}'.format(cdf(19.5)))
+# Between 20 and 22
+print('{:.3f}'.format(cdf(22) - cdf(20)))
