55a8344094305111cc7b8769f7c12ca624445cbf
sharvi24 <43946878+sharvi24@users.noreply.github.com>
2021-04-29 11:27:21 +0530

    Update solution.py

--- Day8: Pearson Correlation Coefficient II/solution.py
+++ Day8: Pearson Correlation Coefficient II/solution.py
@@ -12,3 +12,5 @@ xy=sum([a[i]*b[i] for i in range(5)])
 b= (5*xy-(sum_a*sum_b))/(5*x_square - (sum_a)**2)
 a=mean_b -b*mean_a
 print(round(a + b*80,3))
+
+#The regression line of y on x is 3x+4y+8=0, and the regression line of x on y is 4y+3x+7=0 . What is the value of the Pearson correlation coefficient?
