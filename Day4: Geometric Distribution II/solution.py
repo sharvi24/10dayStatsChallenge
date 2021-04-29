13acdea72b09dc8b87105646de05b03cd1e3fe89
sharvi24 <43946878+sharvi24@users.noreply.github.com>
2021-04-29 13:26:50 +0530

    Update solution.py

--- Day4: Geometric Distribution II/solution.py
+++ Day4: Geometric Distribution II/solution.py
@@ -1,18 +1,7 @@
-a=1.09
-b=1
-p=a/(a+b)
-q=b/(a+b)
+a=1
+b=3
+n=5
+p=a/b
+q=1-p
 
-def factorial(z):
-    n=1
-    for i in range(1,z+1):n=n*i
-    return n 
-
-def perm(x,y):
-    return factorial(x)/(factorial(x-y)*factorial(y))    
-
-prob=0    
-for i in range(3,7):
-    prob=prob+perm(6,i)*(p**i)*(q**(6-i))
-
-print(round(prob,3)) 
+print(round(1-(q**(n)),3))
