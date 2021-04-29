8391967f446dabb3b5fbd7ce260a46e427bcf450
sharvi24 <43946878+sharvi24@users.noreply.github.com>
2021-04-29 13:25:54 +0530

    Update solution.py

--- Day5: Normal Distribution II/solution.py
+++ Day5: Normal Distribution II/solution.py
@@ -1,14 +1,15 @@
-a=[95,85,80,70,60]
-b=[85,95,70,65,70]
 
-sum_a=sum(a)
-mean_a=sum_a/5
-sum_b=sum(b)
-mean_b=sum_b/5
+import math
+mean, std = 70, 10
 
-x_square=sum([i**2 for i in a])
-xy=sum([a[i]*b[i] for i in range(5)])
+def cdf(x):
+    return 0.5 * (1 + math.erf((x - mean) / (std * (2 ** 0.5))))
 
-b= (5*xy-(sum_a*sum_b))/(5*x_square - (sum_a)**2)
-a=mean_b -b*mean_a
-print(round(a + b*80,3))
+# Less than 19.5
+perc1=cdf(60)*100
+perc2=(1-cdf(60))*100
+perc3=perc1
+
+print(round(perc1,2))
+print(round(perc2,2))
+print(round(perc3,2))
